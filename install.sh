f403ba04c49d778316d3533f7b1d51db1e9aa629
ljbitzki <ljbitzki@gmail.com>
2016-05-09 13:50:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+apt-get update
+apt-get install vim ntpdate apache2 git bc dnsutils snmpd -y
+
 GPATH="/var/www/html/graphs"
 cp graphs.sh ntp.sh /etc
 chmod +x /etc/{graphs.sh,ntp.sh}
 
 sed -i '$ d' /etc/crontab
-echo '
-*  *    * * *   root    /etc/graphs.sh
-#
-' >> /etc/crontab
-/etc/init.d/crontab restart
+echo -e '*  *    * * *   root    /etc/graphs.sh
+#' >> /etc/crontab
+
+sed -i '$ d' /etc/rc.local
+echo -e '/etc/ntp &
+exit 0' >> /etc/rc.local
+
+echo -e 'view   systemonly  included   .1' >> /etc/snmp/snmpd.conf
+
 mkdir -p $GPATH/{csv,assets}
 cp index.php base $GPATH
 chown www-data:www:data $GPATH -R
